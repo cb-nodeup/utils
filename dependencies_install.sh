7666e1f782aecfd3d38f1beb0a4a7568aaa344cc
node up <163672600+nodeup-xyz@users.noreply.github.com>
2024-03-16 16:38:21 -0400

    nodejs typo

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -5,4 +5,4 @@ printGreen "2. Updating packages..." && sleep 1
 sudo apt update
 
 printGreen "3. Installing dependencies..." && sleep 1
-sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc node npm unzip -y
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc nodejs npm unzip -y
