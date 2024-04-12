408d60d66af2009b9d8d5c84e93f54af8844d53f
node up <163672600+nodeup-xyz@users.noreply.github.com>
2024-04-12 08:33:21 -0400

    Update dependencies_install.sh

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -2,7 +2,7 @@
 source <(curl -s https://raw.githubusercontent.com/nodeup-xyz/utils/main/common.sh)
 
 printGreen "2. Updating packages..." && sleep 1
-sudo apt update
+sudo apt update && sudo apt upgrade -y
 
 printGreen "3. Installing dependencies..." && sleep 1
 sudo apt install curl tar git wget htop screen build-essential jq make lz4 gcc unzip -y
